fe78ced90438320f91808bd39443670d4ce2b806
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-22 11:41:22 -0700

    add PlaceName property and constructor to Place class, changed test passed
    Co-Authored-By: Erich Richter <erich.richter@gmail.com>

--- PlacesYouHaveBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesYouHaveBeen.Tests/ModelTests/PlaceTests.cs
@@ -10,7 +10,7 @@ namespace PlacesYouHaveBeen.Tests
     [TestMethod]
     public void PlaceConstructor_CreatesInstanceOfPlace_Place()
     {
-      Place newPlace = new Place();
+      Place newPlace = new Place("Portland");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
     }
   }
--- PlacesYouHaveBeen/Models/Place.cs
+++ PlacesYouHaveBeen/Models/Place.cs
@@ -2,6 +2,12 @@ namespace PlacesYouHaveBeen.Models
 {
   public class Place
   {
+    public string PlaceName {get; set;}
+
+    public Place(string name)
+    {
+      PlaceName = name;
+    }
 
   }
 }
