72228ee88a4c01e0bfebeba494b468f167cb3611
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-22 12:02:51 -0700

    add PlaceName and _places list properties, constructor and fucntioms to get and clear the places list
    Co-Authored-By: Erich Richter <erich.richter@gmail.com>

--- PlacesYouHaveBeen/Models/Place.cs
+++ PlacesYouHaveBeen/Models/Place.cs
@@ -1,12 +1,26 @@
+using System.Collections.Generic;
+
 namespace PlacesYouHaveBeen.Models
 {
   public class Place
   {
     public string PlaceName {get; set;}
 
+    private static List<Place> _places = new List<Place>();
+
     public Place(string name)
     {
       PlaceName = name;
+      _places.Add(this);
+    }
+    public static List<Place> GetAll()
+    {
+      return _places;
+    }
+
+    public static void ClearAll()
+    {
+      _places.Clear();
     }
 
   }
