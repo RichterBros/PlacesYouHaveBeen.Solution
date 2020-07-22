39811a6f191c039617eb71128b210d3f2149bdee
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-22 14:38:39 -0700

    add new properties and Find by Id method
    Co-Authored-By: Erich Richter <erich.richter@gmail.com>

--- PlacesYouHaveBeen/Models/Place.cs
+++ PlacesYouHaveBeen/Models/Place.cs
@@ -5,23 +5,31 @@ namespace PlacesYouHaveBeen.Models
   public class Place
   {
     public string PlaceName {get; set;}
-
+    public int NumberOfDays {get; set;}
+    public string Description {get; set;}
+    public int Id{get;}
     private static List<Place> _places = new List<Place>();
 
-    public Place(string name)
+    public static Place Find(int searchId)
+    {
+      return _places[searchId - 1];
+    }
+
+    public Place(string name, int days, string description)
     {
       PlaceName = name;
+      NumberOfDays = days;
+      Description = description;
       _places.Add(this);
+      Id = _places.Count;
     }
     public static List<Place> GetAll()
     {
       return _places;
     }
-
     public static void ClearAll()
     {
       _places.Clear();
     }
-
   }
 }
