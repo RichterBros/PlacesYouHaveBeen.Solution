e5762754878442768ace09f1165bb27a61a5fe8d
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-22 13:35:01 -0700

    add route to Index.cshtml in Views/Home
    Co-Authored-By: Erich Richter <erich.richter@gmail.com>

--- PlacesYouHaveBeen/Controllers/HomeController.cs
+++ PlacesYouHaveBeen/Controllers/HomeController.cs
@@ -5,7 +5,10 @@ namespace PlacesYouHaveBeen.Controllers
 {
   public class HomeController : Controller
   {
-
-
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
